1f41910b04444ff05181c7f2db8ef9e55ddc83e5
Lorencini <arthnilo@gmail.com>
2023-04-28 10:21:23 -0300

    fetch-api-produtos

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 const vm = new Vue({
-    el: "#app",
-    data: {
-        produtos: [],
+  el: "#app",
+  data: {
+    produtos: [],
+    produto: false,
+  },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
     },
-    methods: {
-        fetchProdutos() {
-            fetch("./api/produtos.json")
-            .then(r => r.json())
-            .then(r => {
-                this.produtos = r;
-            })
-        }
+  },
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then((r) => r.json())
+        .then((r) => {
+          this.produtos = r;
+        });
     },
-    created() {
-        this.fetchProdutos();
-    }
-})
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((r) => r.json())
+        .then((r) => {
+          this.produto = r;
+        });
+    },
+  },
+  created() {
+    this.fetchProdutos();
+  },
+});
