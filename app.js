24e1ece517808d261f75fa493eccbcfc5112329e
Lorencini <arthnilo@gmail.com>
2023-05-15 22:56:56 -0300

    modal-behavior

--- app.js
+++ app.js
@@ -27,6 +27,17 @@ const vm = new Vue({
           this.produto = r;
         });
     },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+    fecharModal({target, currentTarget}) {
+      if(target === currentTarget)
+        this.produto = false;
+    },
   },
   created() {
     this.fetchProdutos();
